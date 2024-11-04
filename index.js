2df00cae20964f374a251751322bd7fa8804c3da
owalishawon <owali.shawon@gmail.com>
2024-11-04 12:46:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const { randomCodeId } = require("./random");
 
-/**
- * Default random number generator with type="utf8"
- * For combination with letters, numbers pass type="hex"
- */
-console.log(randomCodeId(15, "TXN-ID-", "-LOL"));
-console.log(randomCodeId(15, "TXN-ID-", "-LOL", "hex"));
+module.exports = { randomCodeId };
